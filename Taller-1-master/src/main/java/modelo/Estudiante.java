54e079c03aeb9716501822295b9b5ae4d9ec6656
HugoFernandezV <104911270+HugoFernandezV@users.noreply.github.com>
2022-05-04 09:58:19 -0400

    Add files via upload

---
+++ Taller-1-master/src/main/java/modelo/Estudiante.java
@@ -0,0 +1,53 @@
+
+package modelo;
+
+/**
+ *
+ * @author Usuario
+ */
+public class Estudiante {
+    private String Nombre;
+    private String Matricula;
+    private String Carrera;
+    private String Correo;
+
+    public Estudiante(String Nombre, String Matricula, String Carrera, String Correo) {
+        this.Nombre = Nombre;
+        this.Matricula = Matricula;
+        this.Carrera = Carrera;
+        this.Correo = Correo;
+    }
+
+    public String getNombre() {
+        return Nombre;
+    }
+
+    public void setNombre(String Nombre) {
+        this.Nombre = Nombre;
+    }
+
+    public String getMatricula() {
+        return Matricula;
+    }
+
+    public void setMatricula(String Matricula) {
+        this.Matricula = Matricula;
+    }
+
+    public String getCarrera() {
+        return Carrera;
+    }
+
+    public void setCarrera(String Carrera) {
+        this.Carrera = Carrera;
+    }
+
+    public String getCorreo() {
+        return Correo;
+    }
+
+    public void setCorreo(String Correo) {
+        this.Correo = Correo;
+    }
+    
+}
